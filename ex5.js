2040deda5c305eb88d3a7cbafeb3efb3ff342daf
buitiendai <buitiendai@gmail.com>
2022-11-14 11:07:20 +0700

    Upload file ex5

---
+++ ex5.js
@@ -0,0 +1,9 @@
+let width = prompt("Nhập vào chiều dài của hình chữ nhật:");
+let height = prompt("Nhập vào chiều rộng của hình chữ nhật:");
+let a = parseInt(width);
+let b = parseInt(height);
+let S = a*b;
+//Hiển thị các giá trị:
+document.write('Chiều dài cạnh hình chữ nhật là: '+ width + 'm' + '<br />');
+document.write('Chiều rộng cạnh hình chữ nhật là: '+ height + 'm' + '<br />');
+document.write('Diện tích hình chữ nhật là: '+ S + 'm2' );
